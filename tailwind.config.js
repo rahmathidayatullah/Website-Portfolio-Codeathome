c1e9307340877dd33d473d11845e0fc5ff119df2
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2021-02-23 19:05:30 +0700

    udate page index home career otw profile dll this night clear

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  purge: [
-      './src/**/*.html',
-     './src/**/*.js',
-  ],
+  purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+
   variants: {},
   plugins: [],
 }
